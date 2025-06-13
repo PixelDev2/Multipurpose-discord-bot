d04d5d516093225825df3f35c71072ecd3b18e22
PixelDev <Sibriham2022@gmail.com>
2025-06-13 21:04:38 +0100

    Create README.ml

---
+++ README.ml
@@ -0,0 +1,3 @@
+# Blue Lock Roblox Scripts for RaulInfinit ⚽💙
+
+Welcome to the official script repository for RaulInfinit's **Blue Lock** Roblox project! This repository contains useful Lua scripts, tools, and utilities to help bring the anime-inspired soccer (football) experience to life on Roblox.
